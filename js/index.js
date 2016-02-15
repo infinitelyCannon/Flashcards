1a98df8b055df4136603955b92416886a39e481c
Dakarai Simmons <infinitelyCannon@gmail.com>
2016-02-15 10:13:00 -0500

    Revamp Interface
    Updating the UI to material standards with Polymer

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var app = app || {};
-var panelTeen = TweenLite.to("#panel", 0.4, {left:0, paused:true});
-var overlayTween = TweenLite.to("#overlay", 0.4, {opacity:0.4, paused:true, onReverseComplete:function(){
-	$("#overlay").css("display","none");
-}});
-
-$(function(){
-	new app.AppView();
-});
